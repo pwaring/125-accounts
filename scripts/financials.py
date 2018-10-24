f989cc79d61b9c39565be858cd19219d9cb37a11
Paul Waring <paul@xk7.net>
2018-10-24 12:05:27 +0100

    Only include current tax year data in financial reporting

--- scripts/financials.py
+++ scripts/financials.py
@@ -23,7 +23,7 @@ with open(config_path + '/tax.yaml') as f:
 
 # Find current tax year
 today = datetime.date.today()
-config['current_tax'] = next(x for x in config['tax'] if x['start_date'] < today and x['end_date'])
+config['current_tax'] = next(x for x in config['tax'] if x['start_date'] <= today and x['end_date'] >= today)
 
 # Data
 total_sales = decimal.Decimal(0.00)
@@ -37,19 +37,21 @@ for invoice_file in invoice_files:
     fp = invoice_file.open()
     invoice_data = yaml.safe_load(fp.read())
     fp.close()
-    print(invoice_data['number'])
 
-    total_sales += decimal.Decimal(invoice_data['total'])
+    if invoice_data['issue_date'] >= config['current_tax']['start_date'] and invoice_data['issue_date'] <= config['current_tax']['end_date'] and invoice_data['issue_date'] <= today:
+        print(invoice_data['number'])
 
-    print(invoice_data['total'])
+        total_sales += decimal.Decimal(invoice_data['total'])
 
-    # Subtract any payments from accounts receivable
-    if 'payments' in invoice_data:
-        for payment in invoice_data['payments']:
-            print(payment['amount'])
-            total_payments += decimal.Decimal(payment['amount'])
+        print(invoice_data['total'])
 
-    print()
+        # Subtract any payments from accounts receivable
+        if 'payments' in invoice_data:
+            for payment in invoice_data['payments']:
+                print(payment['amount'])
+                total_payments += decimal.Decimal(payment['amount'])
+
+        print()
 
 print("Total sales: %.2f" % total_sales)
 print("Total payments: %.2f" % total_payments)
